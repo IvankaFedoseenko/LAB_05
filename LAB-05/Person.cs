633304a7fbcefdbe88455f21722ae7086e88b27f
Ivanna Fedoseenko <ivanna.fedoseenko@microsoft.wsei.edu.pl>
2025-01-10 13:00:37 +0100

    Add project files.

---
+++ LAB-05/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_05
+{
+    // Zadanie 3
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAdress { get; set; }
+        public Person(string firstName, string lastName, DateTime birthDate,
+            string emailAdress)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            EmailAdress = emailAdress;
+
+        }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+
+        public bool IsAdult()
+        {
+            if (DateTime.Now.Year - BirthDate.Year >= 18)
+            {
+                if (DateTime.Now.Month < BirthDate.Month)
+                {
+                    return true;
+                }
+                else if (DateTime.Now.Month == BirthDate.Month)
+                {
+                    if (DateTime.Now.Day <= BirthDate.Day)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            else { return false; }
+        }
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ LAB-05/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace LAB_05
+{
+    internal class Program
+    {
+        public struct Coords
+        {
+            //Zadanie 1
+            public double X { get; }
+            public double Y { get; }
+            public Coords(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void GetCoords(Coords b)
+            {
+                double result = Math.Sqrt((this.X - b.X) * (this.X - b.X) + (this.Y - b.Y) * (this.Y - b.Y));
+                Console.WriteLine(result);
+            }
+            // Zadanie 2
+            static (int Min, int Max) GetMinMax(int[] numbers)
+            {
+                int min = numbers[0];
+                int max = numbers[0];
+                foreach (int number in numbers)
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                    if (number > max)
+                    {
+                        max = number;
+                    }
+                }
+                return (min, max);
+            }
+
+            static void Main(string[] args)
+            {
+                // Zadanie 1
+                Coords coords = new Coords(1.0, 2.0);
+                Coords coords1 = new Coords(3.0, 4.0);
+                coords.GetCoords(coords1);
+
+                //Zadanie 2
+                int[] numbers = { 5, 2, 9, 1, 7, 3, 8 };
+                Console.WriteLine(GetMinMax(numbers));
+
+                //Zadanie 3
+                Person john = new Person("John", "Doe", new DateTime(2000, 02, 12), "johndoe@gmail.com");
+                Person elly = new Person("Elly", "Doe", new DateTime(2013, 12, 12), "ellydoe@gmail.com");
+                Console.WriteLine(john.IsAdult());
+                Console.WriteLine(elly.GetAge());
+
+            }
+        }
+    }
+}
